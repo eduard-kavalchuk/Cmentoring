f6c2841c1500301fc41cbd419ff9ca5a378428f2
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2024-05-18 11:01:53 +0100

    Fixed for leap year

--- Module4/days_of_month.c
+++ Module4/days_of_month.c
@@ -1,17 +1,33 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void days_in_month(int month);
+void days_in_month(int month, int year);
+bool is_leap_year(int year);
 
 int main(void)
 {
-	for (int i = 0; i < 14; i++) {
-		days_in_month(i);
+	char *endptr, buffer[5];
+	int year;
+	
+	printf("Enter year: ");
+	if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+		return -1;  /* Unexpected error */
+
+	year = strtol(buffer, &endptr, 10);
+	if (buffer == endptr) {
+		printf("Year must be an integer number.");
+		return -1;
+	}
+	
+	for (int i = 1; i <= 12; i++) {
+		days_in_month(i, year);
 	}
 	
 	return 0;
 }
 
-void days_in_month(int month)
+void days_in_month(int month, int year)
 {
 	enum { JAN = 1, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC };
 	
@@ -32,10 +48,18 @@ void days_in_month(int month)
 			printf("%d\n", 30);
 			break;
 		case FEB:
-			printf("%d\n", 28);
+			if (is_leap_year(year))
+				printf("%d\n", 29);
+			else
+				printf("%d\n", 28);
 			break;
 		default:
 			printf("Invalid input\n");
 			break;
 	}
+}
+
+bool is_leap_year(int year)
+{
+	return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
 }
