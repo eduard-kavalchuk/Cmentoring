6fcaccd5d02182efc58482db6985365bdbfb7399
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2024-05-18 10:01:46 +0100

    Fixes for task 1

--- Module4/power.c
+++ Module4/power.c
@@ -1,42 +1,41 @@
 #include <stdio.h>
 
-int power_while(int arg, int factor);
-int power_for(int arg, int factor);
+double power_while(int arg, int factor);
+double power_for(int arg, int factor);
 
 int main(void)
 {
-	printf("%d\n", power_while(2, 10));
-	printf("%d\n", power_for(2, 10));
+	printf("Power while:\n");
+	printf("%d ^ %d = %f\n", 2, 0, power_while(2, 0));
+	printf("%d ^ %d = %f\n", 3, -2, power_while(3, -2));
+	printf("%d ^ %d = %f\n", 3, 4, power_while(3, 4));
+	
+	printf("\nPower for:\n");
+	printf("%d ^ %d = %f\n", 2, 0, power_for(2, 0));
+	printf("%d ^ %d = %f\n", 3, -2, power_for(3, -2));
+	printf("%d ^ %d = %f\n", 3, 4, power_for(3, 4));
 	
 	return 0;
 }
 
-int power_while(int arg, int factor)
+double power_while(int arg, int factor)
 {
-	if (factor < 0)
-		return -1;
+	double result = 1.0;
+	double multiplier = factor > 0 ? arg : (factor *= -1, 1.0 / arg);
 	
-	if (0 == factor)
-		return 1;
+	while (--factor >= 0)
+		result *= multiplier;
 	
-	int result = arg;
-	while (--factor > 0) {
-		result *= arg;
-	}
 	return result;
 }
 
-int power_for(int arg, int factor)
+double power_for(int arg, int factor)
 {
-	if (factor < 0)
-		return -1;
+	double result = 1.0;
+	double multiplier = factor > 0 ? arg : (factor *= -1, 1.0 / arg);
 	
-	if (0 == factor)
-		return 1;
-	
-	int result = arg;
-	for (int i = 1; i < factor; i++) {
-		result *= arg;
+	for (int i = 0; i < factor; i++) {
+		result *= multiplier;
 	}
 	
 	return result;
