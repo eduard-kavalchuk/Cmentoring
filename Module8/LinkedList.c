c636719c4950f0efb7950494692fadb7d9dc01a9
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2024-07-20 22:46:17 +0300

    Continued implementation of LinkedList

--- Module8/LinkedList.c
+++ Module8/LinkedList.c
@@ -1,4 +1,4 @@
-// Implement Linked list and insert and delete operations for it.
+// Implement Linked list and append and delete operations for it.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,20 +15,62 @@ struct LinkedListHead {
 };
 
 struct LinkedListHead *init(void);
-void insert(struct LinkedListHead *listHead, int value);
+void append(struct LinkedListHead *listHead, int value);
 void deleteAll(struct LinkedListHead *listHead);
-
+void insert(struct LinkedListHead *listHead, int position, int value);
 void printList(struct LinkedListHead *listHead);
+
 
 int main(void)
 {
     struct LinkedListHead *listHead = init();
     for(int i = 0; i < 10; i++) {
-        insert(listHead, i);
+        append(listHead, i);
     }
     printList(listHead);
     
+    insert(listHead, 4, 11);
+    printList(listHead);
+    
+    insert(listHead, 0, 12);
+    printList(listHead);
+    
+    insert(listHead, listHead->size, 13);
+    printList(listHead);
+    
+    deleteAll(listHead);
+    
     return 0;
+}
+
+void insert(struct LinkedListHead *listHead, int position, int value)
+{
+    if (listHead->size < position)
+        return;
+    
+    if (position == 0) {
+        struct LinkedList *list = (struct LinkedList *) malloc(sizeof(struct LinkedList));
+        list->value = value;
+        list->next = listHead->first;
+        listHead->first = list;
+    }
+    else if (listHead->size == position) {
+        struct LinkedList *list = (struct LinkedList *) malloc(sizeof(struct LinkedList));
+        list->value = value;
+        listHead->last->next = list;
+        listHead->last = list;
+    }
+    else {
+        struct LinkedList *list = (struct LinkedList *) malloc(sizeof(struct LinkedList));
+        list->value = value;
+        struct LinkedList *tmp = listHead->first;
+        for (int i = 0; i < position - 1; i++)
+            tmp = tmp->next;
+        list->next = tmp->next;
+        tmp->next = list;
+    }
+    
+    
 }
 
 struct LinkedListHead *init(void)
@@ -40,7 +82,7 @@ struct LinkedListHead *init(void)
     return head;
 }
 
-void insert(struct LinkedListHead *listHead, int value)
+void append(struct LinkedListHead *listHead, int value)
 {
     struct LinkedList *list = (struct LinkedList *) malloc(sizeof(struct LinkedList));
     list->value = value;
@@ -67,5 +109,13 @@ void printList(struct LinkedListHead *listHead)
     printf("\n");
 }
 
-
+void deleteAll(struct LinkedListHead *listHead)
+{
+    struct LinkedList *ptr = listHead->first;
+    while(ptr != NULL) {
+        struct LinkedList *tmp = ptr;
+        free(ptr);
+        ptr = tmp->next;
+    }
+}
 
